96c329b62d22c32ccf136846919a38acf8187ad9
zaki4594 <Amirul_4594@yahoo.com>
2016-11-19 18:43:41 +0100

    Update DeCosmogonie.java

--- Pandocreon Divinae/src/projet/joueur/DeCosmogonie.java
+++ Pandocreon Divinae/src/projet/joueur/DeCosmogonie.java
@@ -2,22 +2,24 @@ package projet.joueur;
 import java.util.Random;
 public class DeCosmogonie {
 	
-	static face resFace;
-	static int faceAleatoire = new Random().nextInt(face.values().length); //ça va nous donner la face du dé aléatoirement
+	private face resFace;
+	private int faceAleatoire = new Random().nextInt(face.values().length); //ça va nous donner la face du dé aléatoirement
 	
 	public enum face{//donne les valeurs aux joueurs 
 		Jour,Nuit,Neant
 	}
+	
 
-	public static face resultatLancement(){
+	public String resultatLancement(){
 		
 		resFace=face.values()[faceAleatoire];//pour aggreger le nom du variable utilisé
-		return  resFace;
+		//System.out.println( resFace);
+		return resFace.name();
 		
 	}
 	
 	public void donnePtActionJour(){
-		System.out.println("Hello");
+		
 		
 	}
 	
