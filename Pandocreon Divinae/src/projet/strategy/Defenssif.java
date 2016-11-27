30223d4f9dfe66e23d0572c2cf2441d830727c9a
zaki4594 <Amirul_4594@yahoo.com>
2016-11-27 23:59:15 +0100

    Update Defenssif.java

--- Pandocreon Divinae/src/projet/strategy/Defenssif.java
+++ Pandocreon Divinae/src/projet/strategy/Defenssif.java
@@ -3,6 +3,10 @@
  */
 package projet.strategy;
 
+import projet.cartes.Carte;
+import projet.cartes.Tapis;
+import projet.joueur.JoueurVirtuel;
+
 /**
  * @author sweet
  *
@@ -14,5 +18,113 @@ public class Defenssif implements Strategy{
 		// TODO Auto-generated method stub
 		return "Defenssif";
 	}
+
+	@Override
+	public int pose_carte(JoueurVirtuel joueur) {
+		// TODO Auto-generated method stub
+		int id = 0;
+		if(Tapis.getCartesCroyantes()>0){
+		for(int i=0;i< joueur.getLaMain().getListeCartesMain().size();i++){	
+			Carte c= joueur.getLaMain().getListeCartesMain().get(i);
+			if(c.getType()=="GuideSpirituel"){//il va prendre le premier carte deusex trouvé dans la liste
+				if(c.getOrigine()=="Jour"){
+					if(joueur.getPtActionJour()>0){
+						id= c.getIdCarte();
+					}
+					
+				}
+				if(c.getOrigine().equals("Nuit")){
+					if(joueur.getPtActionNuit()>0){
+						id= c.getIdCarte();
+					}
+					
+				}
+				if(c.getOrigine().equals("Neant")){
+					if(joueur.getPtActionNeant()>0){
+						id= c.getIdCarte();
+					}
+					if(joueur.getPtActionJour()>1){
+						id= c.getIdCarte();
+					}
+					if(joueur.getPtActionNuit()>1){
+						id= c.getIdCarte();
+					}
+					
+					
+				}
+			}
+			}
+		}
+		else{
+			for(int i=0;i< joueur.getLaMain().getListeCartesMain().size();i++){	
+				Carte c= joueur.getLaMain().getListeCartesMain().get(i);
+				if(c.getType()=="Croyants"){//il va prendre le premier carte deusex trouvé dans la liste
+					if(c.getOrigine()=="Jour"){
+						if(joueur.getPtActionJour()>0){
+							id= c.getIdCarte();
+						}
+						
+					}
+					if(c.getOrigine().equals("Nuit")){
+						if(joueur.getPtActionNuit()>0){
+							id= c.getIdCarte();
+						}
+						
+					}
+					if(c.getOrigine().equals("Neant")){
+						if(joueur.getPtActionNeant()>0){
+							id= c.getIdCarte();
+						}
+						if(joueur.getPtActionJour()>1){
+							id= c.getIdCarte();
+						}
+						if(joueur.getPtActionNuit()>1){
+							id= c.getIdCarte();
+						}
+						
+						
+					}
+				}
+				if(c.getType()=="DeusEx"){//il va prendre le premier carte deusex trouvé dans la liste
+					if(c.getOrigine()=="Jour"){
+						if(joueur.getPtActionJour()>0){
+							id= c.getIdCarte();
+						}
+						
+					}
+					if(c.getOrigine().equals("Nuit")){
+						if(joueur.getPtActionNuit()>0){
+							id= c.getIdCarte();
+						}
+						
+					}
+					if(c.getOrigine().equals("Neant")){
+						if(joueur.getPtActionNeant()>0){
+							id= c.getIdCarte();
+						}
+						if(joueur.getPtActionJour()>1){
+							id= c.getIdCarte();
+						}
+						if(joueur.getPtActionNuit()>1){
+							id= c.getIdCarte();
+						}
+						
+						
+					}
+					if(c.getOrigine().equals("None")){
+						id=c.getIdCarte();
+					}
+				}
+				else{
+					id=c.getIdCarte();
+				}
+				break;
+				}
+		}
+		return id;
+		
+	}
+
+
 
 }
