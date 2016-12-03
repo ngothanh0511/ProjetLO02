103b2252771c6d96f7c71447a5c71ff09462972d
zaki4594 <Amirul_4594@yahoo.com>
2016-12-03 15:16:36 +0100

    Update Strategy.java

--- Pandocreon Divinae/src/projet/strategy/Strategy.java
+++ Pandocreon Divinae/src/projet/strategy/Strategy.java
@@ -1,6 +1,10 @@
 package projet.strategy;
 
+import projet.cartes.StockCarte;
+import projet.joueur.JoueurVirtuel;
+
 public interface Strategy {
 	String mode();
 	int pose_carte(JoueurVirtuel joueur);
+	void defausser_carte(JoueurVirtuel joueur, StockCarte s);
 }
